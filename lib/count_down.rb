88c44509b0581c5ee32ef57499341d1478937046
malcolm staso <mstaso1@gmail.com>
2020-05-04 20:44:41 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,8 @@
 # Write your code here
-happy_new_year=10  
-count=0
-while count < 10 do 
+happy_new_year=0  
+count=10
+while happy_new_year < 10 do 
   puts "#{count}"
-  count += 1
-  if count == 10
-    
+  happy_new_year -= 1
+  
 end  
